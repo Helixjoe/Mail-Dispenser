e7b9e0a684846e7b72a89121f7c3f121a3488fb8
Joel John <joeljohn5112@gmail.com>
2023-02-06 23:53:21 +0530

    Basic express server initiliazed

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-// Reading data from file
-const inputElement = document.getElementById(".fileInput");
-inputElement.addEventListener("change", handleFileSelect, false);
-function handfiles() {
-  const fileList = this.files;
-}
-const numFiles = fileList.length;
-console.log(numFiles);
+// // Reading data from file
+// const inputElement = document.getElementById(".fileInput");
+// inputElement.addEventListener("change", handleFileSelect, false);
+// function handfiles() {
+//   const fileList = this.files;
+// }
+// const numFiles = fileList.length;
+// console.log(numFiles);
+
+const express = require("express");
+
+const app = express();
+
+app.listen(3000, function () {
+  console.log("Server is running successfully on port 3000");
+});

2a92ffea2cd0833c00c62f33e24430dbd93904c0
Joel John <joeljohn5112@gmail.com>
2023-02-07 23:47:15 +0530

    basic html ready and forms attached to js

--- app.js
+++ app.js
@@ -8,9 +8,17 @@
 // console.log(numFiles);
 
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
-
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", function (req, res) {
+  res.sendFile("/index.html");
+});
+app.post("/email", function (req, res) {
+  const emailId = req.body.email;
+  res.send("Is your email " + emailId + "?");
+});
 app.listen(3000, function () {
   console.log("Server is running successfully on port 3000");
 });

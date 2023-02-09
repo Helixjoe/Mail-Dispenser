6bfd3c4e00f4dca625248088d7f4bf1282f790b5
Joel John <joeljohn5112@gmail.com>
2023-02-09 19:35:38 +0530

    middleware code added

--- app.js
+++ app.js
@@ -9,15 +9,28 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const multer = require("multer");
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+// Multer file storage
+const Storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, "./attachments");
+  },
+  filename: function (req, file, callback) {
+    callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
+  },
+});
+// Middleware to get a single attachment
+const attachmentUpload = multer({
+  storage: Storage,
+}).single("attachment");
 app.get("/", function (req, res) {
   res.sendFile("/index.html");
 });
 app.post("/email", function (req, res) {
-  const emailId = req.body.email;
-  res.send("Is your email " + emailId + "?");
+  console.log(req.body);
 });
 app.listen(3000, function () {
   console.log("Server is running successfully on port 3000");

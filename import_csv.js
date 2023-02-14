e60b338d718175b78a251c65d9854caeafb260ac
Joel John <joeljohn5112@gmail.com>
2023-02-14 23:16:51 +0530

    valid emails stored in js memory

--- import_csv.js
+++ import_csv.js
@@ -7,6 +7,8 @@ app.use(express.static("public"));
 //setting up body-parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+let valid = [];
+let invalid = [];
 app.get("/", function (req, res) {
   res.sendFile("/index.html");
 });
@@ -23,20 +25,18 @@ app.post("/upload", upload.single("file"), function (req, res) {
     data.push(values);
   }
   //email validation system
-  let valid = [];
-  let invalid = [];
+
   for (let i = 1; i < data.length; i++) {
     let email = data[i][0].trimEnd();
     let verdict = validator.is_email_valid(email);
-    if (verdict == true) {
+    if (verdict) {
       valid.push(email);
     } else {
       invalid.push(email);
     }
   }
   //creating HTML Table
-  let htmlTable =
-    "<body><link rel='stylesheet' href='/styles.css' /><h1 class='heading'>Emails</h1><table>";
+  let htmlTable = "<h1 class='heading'>Emails</h1><table>";
   htmlTable +=
     "<tr><td><h2>Valid Emails<h2></td><td><h2>Invaild Emails</h2></td></tr>";
   for (let i = 0; i < valid.length; i++) {
@@ -48,8 +48,13 @@ app.post("/upload", upload.single("file"), function (req, res) {
     }
     htmlTable += "</td></tr>";
   }
-  htmlTable += "</table></body>";
-  res.sendFile(__dirname + "/filter.html");
+  htmlTable += "</table>";
+  // res.sendFile(__dirname + "/public/filter.html");
+  res.send(htmlTable);
+});
+
+app.get("/email", function (res, req) {
+  console.log(valid);
 });
 app.listen(3000, function () {
   console.log("Server started");
